a0f0dba3966e4405dac29492b0cd639070982a35
lord zord07 <zord0074@gmail.com>
2024-03-01 14:07:36 +0700

    work wersion

--- Classes/SimpleScaner.cs
+++ Classes/SimpleScaner.cs
@@ -6,12 +6,27 @@ using System.Text;
 using System.Threading.Tasks;
 using CSharpTFLLab.Enums;
 using System.Windows.Forms;
+using System.Runtime.Serialization.Formatters;
 
 namespace CSharpTFLLab.Classes
 {
+    internal struct Word
+    {
+        internal Word(StructScanEnum StructScanEnum, string str, int start, int end)
+        {
+            this.StructScanEnum = StructScanEnum;
+            this.str = str;
+            this.start = start;
+            this.end = end;
+        }
+        internal StructScanEnum StructScanEnum;
+        internal string str;
+        internal int start;
+        internal int end;
+    }
     internal class SimpleScaner : IScaner
     {
-        private string[,] _structScanDescription = { { "ошибка", ""},
+        private string[,] _structScanDescription = {{ "", ""},
                                                     { "ключевое слово", "struct" },
                                                     { "ключевое слово", "int" },
                                                     { "ключевое слово", "bool" },
@@ -22,7 +37,8 @@ namespace CSharpTFLLab.Classes
                                                     { "открывающая скобка", "{" },
                                                     { "конец оператора", ";" },
                                                     { "закрывающая скобка", "}" }};
-        char [] _unicChr = { ' ', '{', '}', ';', '\n', '\t'};
+        char[] _spaceChars = { ' ', '\n', '\t' };
+        char[] _specialChars = { '{', ';', '}' };
         internal SimpleScaner(MainForm form)
         {
             _form = form;
@@ -42,137 +58,80 @@ namespace CSharpTFLLab.Classes
             //Filter();
             CheckProcess();
         }
-        private (StructScanEnum StructScanEnum, string str, int start, int end) GetNextWord()
+        private Word GetNextWord()
         {
-            if(_curChr >= _buffer.Length)
-                return (StructScanEnum.ERROR, "", _buffer.Length, _buffer.Length);
             int start = _curChr;
-            int end =buffer.IndexOfAny(_unicChr, _curChr);
-            if(end == -1)
-                end = _buffer.Length;
-            if (start == end)
-                end++;
-            _curChr = end;
-            _curColumn += end - start;
-            string subString = buffer.Substring(start, _curChr - start);
-            //_curChr++;
-            int newLine = subString.Count(x => x == '\n');
-            if(newLine != 0)
+            while (_curChr < _buffer.Length && Char.IsLetterOrDigit(_buffer[_curChr]))
+                _curChr++;
+            if(start == _curChr)
             {
-                _curLine += newLine;
-                _curColumn = end - start;
+                if (_curChr == _buffer.Length)
+                    return new Word(StructScanEnum.NONE, "", start, _curChr);
+                else
+                    _curChr += 1;
             }
-            subString = subString.Trim(new char[]{ '\n', '\t' });
-            int code = 0;
-            //Если это Уникальный символ
-            if(subString.Length == 1)
+            int end = _curChr;
+            string subString = _buffer.Substring(start, end - start);
+            _curColumn += subString.Length;
+            if(subString.Length == 1 && !(Char.IsLetter(subString[0]) || _specialChars.Contains(subString[0])))
             {
-                code = 10;
-                for (; code >= 7; code--)
-                    if (_structScanDescription[code, 1] == subString)
-                        break;
-            }//Если это длинное слово
-            else
-            {
-                code = 1;
-                //Ищем ключевое слово
-                for(; code <6; code++)
-                    if (_structScanDescription[code, 1] == subString)
-                        break;
-                //Иначе - идентификатор
+                if (_spaceChars.Contains(subString[0]))
+                {
+                    if (subString[0] == '\n')
+                    { 
+                        _curLine++;
+                        _curColumn = 0;
+                    }
+                    else
+                        _curColumn++;
+                    return new Word(StructScanEnum.Space, subString, start, end);
+                }
+                else
+                    return new Word(StructScanEnum.ERROR, subString, start, end);
             }
-            return ((StructScanEnum)code, subString, start, end);
+            for (int i = 1; i < _structScanDescription.GetLength(0); i++)
+            {
+                if (i == 6) i++;
+                if (subString == _structScanDescription[i, 1])
+                    return new Word((StructScanEnum)i, subString, start, end);
+            }
+            return new Word(StructScanEnum.Identificatory, subString, start, end);
         }
-        private void SkipSpace(ref (StructScanEnum StructScanEnum, string str, int start, int end) word)
+        private void NextWordAndSkipSpace(ref Word word)
         {
-            while (word.StructScanEnum == StructScanEnum.Space && _curChr < _buffer.Length)
+            do
             {
-                OutputResult((int)word.StructScanEnum, _structScanDescription[(int)word.StructScanEnum, 0], word.str, word.start, word.end);
                 word = GetNextWord();
-            }
+                OutputResult(word);
+            } while (word.StructScanEnum == StructScanEnum.Space && (word.StructScanEnum != StructScanEnum.NONE || word.StructScanEnum == StructScanEnum.ERROR));
         }
         private void CheckProcess()
         {
             _step = StructStepEnum.Search;
             int levelDeep = 0;
-            while(_curChr < _buffer.Length)
+            Word word;
+            do
             {
-                (StructScanEnum StructScanEnum, string str, int start, int end) word = GetNextWord();
-                SkipSpace(ref word);
-                switch (_step)
-                {
-                    case StructStepEnum.Search:
-                        if (word.StructScanEnum == StructScanEnum.Struct)
-                        {
-                            _step = StructStepEnum.KeywordStructure;
-                        }
-                        else if (((int)word.StructScanEnum) >= 2 && ((int)word.StructScanEnum) <= 5)
-                        {
-                            _step = StructStepEnum.KeywordType;
-                        }
-                        else if(levelDeep -1 >= 0 && word.StructScanEnum == StructScanEnum.CloseBracket)
-                        {
-                            _step = StructStepEnum.CloseBracket;
-                            levelDeep--;
-                        }
-                        else if (word.StructScanEnum == StructScanEnum.Identificatory)
-                            OutputError("Работа с переменными пока не реализована");
-                        else
-                            OutputError("Ожидается ключевое слово"); 
-                        break;
-                    case StructStepEnum.KeywordStructure:
-                        if (word.StructScanEnum != StructScanEnum.Identificatory)
-                            OutputError("Ожидается идентификатор структуры");
-                        else
-                        {
-                            OutputResult((int)word.StructScanEnum, _structScanDescription[(int)word.StructScanEnum, 0], word.str, word.start, word.end);
-                            SkipSpace(ref word); 
-                            word = GetNextWord();
-                            if (word.StructScanEnum == StructScanEnum.OpenBracket)
-                            {
-                                _step = StructStepEnum.Search;
-                                levelDeep++;
-                            }
-                            else
-                                OutputError("Ожидается открывающая скобка");
-                        }
-                        break;
-                    case StructStepEnum.KeywordType:
-                        if (word.StructScanEnum != StructScanEnum.Identificatory)
-                            OutputError("Ожидается идентификатор переменной");
-                        else
-                        {
-                            OutputResult((int)word.StructScanEnum, _structScanDescription[(int)word.StructScanEnum, 0], word.str, word.start, word.end);
-                            SkipSpace(ref word);
-                            word = GetNextWord();
-                            if (word.StructScanEnum == StructScanEnum.EndOperator)
-                                _step = StructStepEnum.Search;
-                        }
-                        break;
-                    case StructStepEnum.CloseBracket:
-                        if (word.StructScanEnum != StructScanEnum.EndOperator)
-                            OutputError("Ожидается завершающий оператор ;");
-                        else
-                        {
-                            levelDeep--;
-                            _step = StructStepEnum.Search;
-                        }
-                        break;
-                    default:
-                        OutputError("Недопустимая лексема");
-                        break;
-                }
-                OutputResult((int)word.StructScanEnum, _structScanDescription[(int)word.StructScanEnum, 0], word.str, word.start, word.end);
-            }
+                word = GetNextWord();   
+                OutputResult(word);
+            }while(word.StructScanEnum != StructScanEnum.NONE && word.StructScanEnum != StructScanEnum.ERROR);
+            if (word.StructScanEnum == StructScanEnum.ERROR)
+                OutputError("Ошибка ликсемы - " + word.str);
         }
 
-        private void OutputResult(int code, string description, string str, int start, int end)
+        private void OutputResult(Word word)
         {
+            if (word.StructScanEnum == StructScanEnum.ERROR || word.StructScanEnum == StructScanEnum.NONE)
+                return;
             if (_form.OutputTextBox.Text.Length > 0)
                 _form.OutputTextBox.AppendText("\n");
-            if (str == " ")
-                str = "(пробел)";
-            _form.OutputTextBox.AppendText($"{code} - {description} - {str} - c {start} по {end} символ");
+            if (word.str == " ")
+                word.str = "(пробел)";
+            else if (word.str == "\n")
+                word.str = "(конец строки)";
+            else if (word.str == "\t")
+                word.str = "(табуляция)";
+            _form.OutputTextBox.AppendText($"{(int)word.StructScanEnum} - { _structScanDescription[(int)word.StructScanEnum, 0]} - {word.str} - c {word.start} по {word.end} символ");
         }
 
         private void OutputError(string massage)
@@ -182,7 +141,7 @@ namespace CSharpTFLLab.Classes
             row.Cells[0].Value = _form.LogDataGrid.Rows.Count - 1;
             row.Cells[1].Value = _form.fileManager.curFile;
             row.Cells[2].Value = _curLine;
-            row.Cells[3].Value = _curChr;
+            row.Cells[3].Value = _curColumn;
             row.Cells[4].Value = massage;
             throw new Exception("Ошибка проверки синтаксиса");
         }
